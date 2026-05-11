33ad2c72ca3c51bd089e3e406a838a280a2c1c08
Behzod Hamdamov <bekzodh332@gmail.com>
2026-05-11 19:58:48 +0500

    fix: add logout

--- src/api/apiLogout.js
+++ src/api/apiLogout.js
@@ -7,11 +7,10 @@ import { tokenService, Toast } from "@utils"
 export const apiLogout = async () => {
   try {
     const { data } = await api.get(endpoints.auth.logout)
-    if (data?.success) {
-      tokenService.clearTokens()
-      window.location.href = '/login'
-      Toast.success("Akkauntdan chiqdingiz !")
-    }
+    tokenService.clearTokens()
+    window.location.href = '/login'
+    Toast.success("Akkauntdan chiqdingiz !")
+    return data
   } catch (error) {
     Toast.error(error?.response?.data?.error?.message)
   }
